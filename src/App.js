56210ac7ab3aea404c0e6f016f3d48752e8d5a35
markohautala <markohautala@live.se>
2024-07-30 15:32:03 +0200

    Import react-bootstrap and test it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button variant="primary">Primary</Button>
       </header>
     </div>
   );
