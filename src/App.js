187b593fcb043488cb5401b07f7534f759c26f56
markohautala <markohautala@live.se>
2024-07-31 12:03:17 +0200

    Add custom styling for the navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
 import NavigationBar from './components/NavigationBar';
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <NavigationBar />
     </div>
   );
--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -3,10 +3,11 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import logo2 from "../assets/logo2.png";
+import styles from "../styles/NavigationBar.module.css";
 
 function NavigationBar() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary" fixed="top">
+    <Navbar expand="lg" className={styles.NavigationBar} fixed="top">
       <Container>
         <Navbar.Brand href="#home">
           <img
@@ -22,13 +23,13 @@ function NavigationBar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link>
+            <Nav.Link href="#home" className={styles.NavLink}>
               <span className="material-symbols-outlined">home</span> Home
             </Nav.Link>
-            <Nav.Link>
+            <Nav.Link href="#login" className={styles.NavLink}>
               <span className="material-symbols-outlined">key</span> Login
             </Nav.Link>
-            <Nav.Link>
+            <Nav.Link href="#create-account" className={styles.NavLink}>
               <span className="material-symbols-outlined">person_add</span> Create Account
             </Nav.Link>
           </Nav>
