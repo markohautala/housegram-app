c56e9e36357d4003a528c44a59952a1ce5066b54
markohautala <markohautala@live.se>
2024-07-31 14:24:12 +0200

    Add React router and add the correct paths

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import NavigationBar from './components/NavigationBar';
-import styles from './App.module.css';
+import NavigationBar from "./components/NavigationBar";
+import styles from "./App.module.css";
+import Container from "react-bootstrap/Container";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className={styles.App}>
       <NavigationBar />
+      <Container className={styles.Main}>
+        <Routes>
+          <Route path="/login" element={<h1>Login here</h1>} />
+          <Route path="/create-account" element={<h1>Create Account</h1>} />
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="*" element={<h1>404 Not Found</h1>} />
+        </Routes>
+      </Container>
     </div>
   );
 }
--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -4,34 +4,37 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import logo2 from "../assets/logo2.png";
 import styles from "../styles/NavigationBar.module.css";
+import { NavLink } from "react-router-dom";
 
 function NavigationBar() {
   return (
     <Navbar expand="lg" className={styles.NavigationBar} fixed="top">
       <Container>
-        <Navbar.Brand href="#home">
-          <img
-            src={logo2}
-            alt="logo"
-            height={50}
-            style={{
-              borderRadius: "50%", // This makes the image round
-              objectFit: "cover", // This ensures the image covers the area without distortion
-            }}
-          />
+        <Navbar.Brand>
+          <NavLink to="/">
+            <img
+              src={logo2}
+              alt="logo"
+              height={50}
+              style={{
+                borderRadius: "50%", // Makes the image round
+                objectFit: "cover", // Ensures the image covers the area without distortion
+              }}
+            />
+          </NavLink>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="#home" className={styles.NavLink}>
+            <NavLink to="/" className={styles.NavLink} end>
               <span className="material-symbols-outlined">home</span> Home
-            </Nav.Link>
-            <Nav.Link href="#login" className={styles.NavLink}>
+            </NavLink>
+            <NavLink to="/login" className={styles.NavLink}>
               <span className="material-symbols-outlined">key</span> Login
-            </Nav.Link>
-            <Nav.Link href="#create-account" className={styles.NavLink}>
+            </NavLink>
+            <NavLink to="/create-account" className={styles.NavLink}>
               <span className="material-symbols-outlined">person_add</span> Create Account
-            </Nav.Link>
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
